753d193a62bf93357ddb1d0e6276ec7b6ec40ae4
jonnii <me@jonnii.com>
2025-12-14 12:55:57 -0500

    pass through

--- cmd/stackit/main.go
+++ cmd/stackit/main.go
@@ -7,6 +7,11 @@ import (
 )
 
 func main() {
+	// Check for passthrough commands before processing with cobra
+	if cli.HandlePassthrough(os.Args) {
+		return // HandlePassthrough already exited
+	}
+
 	rootCmd := cli.NewRootCmd()
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
